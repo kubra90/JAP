8d8f488ce0eabf9884bbbe4d4d69f690cf06bed8
kubra <kubracetin.ist@gmail.com>
2023-07-20 20:15:51 -0400

    updated

--- src/main/java/com/example/hibexample/controller/AppController.java
+++ src/main/java/com/example/hibexample/controller/AppController.java
@@ -2,8 +2,10 @@ package com.example.hibexample.controller;
 
 import com.example.hibexample.data.ProductRepository;
 import com.example.hibexample.model.Product;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class AppController {
@@ -11,23 +13,27 @@ public class AppController {
 
     //TODO: Inject repositories
 
+    @Autowired
     private ProductRepository prodRepo;
-    
+
 
 
     @GetMapping("/products")
     public List<Product> getAllProducts() {
 
         //TODO: Call the right repository method
-
-        return  null;
+        return prodRepo.findAll();
     }
 
     @GetMapping("/product/{id}")
     public Product getProduct(@PathVariable long id) {
 
         //TODO: Call the right repository method
-
+        // findByid return Optional object, it is like null checker!
+        Optional<Product> product= prodRepo.findById(id);
+        if(product.isPresent()){
+            return product.get();
+        }
         return null;
     }
 
@@ -57,6 +63,13 @@ public class AppController {
     public void updateProduct(@RequestBody Product product, @PathVariable long id) {
 
         //TODO: Call the right repository method
+        //first of all, we're going to check the object with specific variable exist.
+       Optional<Product> productOpt = prodRepo.findById(id);
+
+       if(productOpt.isPresent()){
+           prodRepo.save(product);
+
+       }
     }
 
     /*
